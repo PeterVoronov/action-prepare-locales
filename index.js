773171780f1b97472befce062900c8449e8bcb91
Peter Voronov <peter.voronov@gmail.com>
2022-11-27 15:46:01 +0200

    added `'isomorphic-git` and basic git checks of files

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const core = require('@actions/core');
+const path = require('path')
+const git = require('isomorphic-git')
+const fs = require('fs')
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const 
-    folderWithSimpleJSONs = core.getInput('folder_with_simple_jsons'),
-    folderWithCoreTranslations = core.getInput('folder_with_core_translations');
+    let  folderWithSimpleJSONs, folderWithCoreTranslations;
+    try {
+      folderWithSimpleJSONs = core.getInput('folder_with_simple_jsons', { required: true });
+    } catch (error) {
+      folderWithSimpleJSONs = 'locales/source'
+    }
+    try {
+      folderWithCoreTranslations = core.getInput('folder_with_core_translations', { required: true });
+    } catch (error) {
+      folderWithCoreTranslations = 'locales'
+    }
     console.log(`folderOfSimpleJSONs ${folderWithSimpleJSONs}!`);
     console.log(`folderOfCoreTranslations ${folderWithCoreTranslations}!`);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
+
+    const 
+      sourcesPath = path.join(process.cwd(),  ...folderWithSimpleJSONs.split('/')),
+      sourcesArray = fs.readdirSync(sourcesPath, {withFileTypes: true})
+        .filter(item => !item.isDirectory())
+        .map(item => item.name);
+    for (const sourceTranslationFile of sourcesArray) {
+      let status = await git.status({ fs, dir: process.cwd(), filepath: [folderWithSimpleJSONs, sourceTranslationFile].join('/') })
+      console.log(`${sourceTranslationFile} status = ${status}`)  
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
